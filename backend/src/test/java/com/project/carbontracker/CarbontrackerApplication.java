3017e6d2a824ad61a518c57ce2085e0afbe5d2a8
vamsikrishna-18 <vamsinalluri555@gmail.com>
2026-08-13 00:11:42 +0530

    Create CarbontrackerApplication class for Spring Boot

---
+++ backend/src/test/java/com/project/carbontracker/CarbontrackerApplication.java
@@ -0,0 +1,12 @@
+package com.project.carbontracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbontrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarbontrackerApplication.class, args);
+    }
+}
